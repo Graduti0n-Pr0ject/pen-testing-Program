25e93ab8ae01faee89abd28fb32617890dd569bc
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2022-11-01 19:25:05 +0200

    fix some issues & and add URL load of github

--- Scripts/Index.py
+++ Scripts/Index.py
@@ -1,13 +1,18 @@
-from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import sys
 import logs_rc
-
+import webbrowser
 class  MainWindow(QtWidgets.QMainWindow):
     def __init__(self) -> None:
         super(MainWindow, self).__init__()
         uic.loadUi("../GUI/Test.ui", self)   # Load Design File
-        # self.setWindowFlag(QtCore.Qt.FramelessWindowHint)                
+        # self.setWindowFlag(QtCore.Qt.FramelessWindowHint)     
+        gitbtn = self.findChild(QtWidgets.QPushButton, "Githubbtn") # find object in gui
+        # load Github URL
+        gitbtn.clicked.connect(lambda x: webbrowser.open_new_tab('https://github.com/Graduti0n-Pr0ject/pen-testing-Program'))
         self.show() # GUI window
+        
+
 
 
 
