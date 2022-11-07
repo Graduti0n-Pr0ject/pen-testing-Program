c5bbe5122038ed66e00f46cb3f726ccbf056e8e8
AbdullahSaidAbdeaaziz <gitHubjoe2@gmail.com>
2022-11-07 16:38:31 +0200

    change way

--- Scripts/Index.py
+++ Scripts/Index.py
@@ -1,21 +1,29 @@
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import sys
-import logs_rc
+import logs_rc  # load icons in Gui
 import webbrowser
-class  MainWindow(QtWidgets.QMainWindow):
+
+
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self) -> None:
         super(MainWindow, self).__init__()
-        uic.loadUi("../GUI/Test.ui", self)   # Load Design File
-        # self.setWindowFlag(QtCore.Qt.FramelessWindowHint)     
-        gitbtn = self.findChild(QtWidgets.QPushButton, "Githubbtn") # find object in gui
+        self.init_ui()
+
+    def init_ui(self):
+        uic.loadUi("../GUI/DarkTheme.ui", self)  # Load Design File
+        # self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        gitbtn = self.findChild(QtWidgets.QPushButton, "Githubbtn")  # find object in gui
         # load Github URL
-        gitbtn.clicked.connect(lambda x: webbrowser.open_new_tab('https://github.com/Graduti0n-Pr0ject/pen-testing-Program'))
-        self.show() # GUI window
-        
+        gitbtn.clicked.connect(lambda x: webbrowser.open_new_tab(
+            'https://github.com/Graduti0n-Pr0ject/pen-testing-Program'))
+        self.show()  # GUI window
 
 
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Start App
+    window = MainWindow()
+    app.exec()  # Exit app when Press X
 
 
-app = QtWidgets.QApplication(sys.argv) # Start App
-window = MainWindow()
-app.exec() # Exit app when Press X
+if __name__ == "__main__":
+    main()
