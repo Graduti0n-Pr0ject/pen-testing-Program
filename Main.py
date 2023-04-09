3f0e812c5a2af6c054564a54bca9af03b71839aa
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-04-09 04:01:41 +0200

    files upload. solve validation wait for reset of tools.

--- Main.py
+++ Main.py
@@ -1,25 +1,67 @@
 import sys
 import os
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QMainWindow, QApplication, QStackedWidget, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QApplication, QStackedWidget, QFileDialog, QMessageBox, QLineEdit, QRadioButton
 import logo_rc
+from Recon.recon import *
+
+
+class Thread(QThread):
+    finished = pyqtSignal()
+
+    def __init__(self, domain=None):
+        self.domain = domain
+
+    def run(self) -> None:
+        super().sleep(1)
+        subfinder_for_single_windows(self.domain)
+        self.finished.emit()
+
 
 
 class MainWindow(QMainWindow):
     path: str = None
     selected_directory: str = None
+
     def __init__(self):
         super().__init__()
+        self.thread = None
         loadUi("design.ui", self)
-        self.widget.hide()
+        # ----------- Home ------------ #
+        self.chooseProject.hide()
+        self.widget_list_domain.hide()
+        self.widget_single_domain.hide()
         self.WAFbtn.clicked.connect(self.do_work)
         self.BackHomebtn.clicked.connect(self.back_home)
         self.BackHomebtn_2.clicked.connect(self.back_home)
         self.filebtn.clicked.connect(self.open_file_dialog)
 
+        # ------------- Recon ---------- #
+
+        self.reconbtn.clicked.connect(self.start_single_list_task)
+
+    def start_single_list_task(self):
+        target: QLineEdit = self.target_line
+        if self.singleRadio.isChecked():
+            if target.text().strip() == '':
+                QMessageBox.information(self, 'Information', f'Enter Right Target Plz')
+            else:
+                self.reconbtn.setEnabled(False)
+                self.thread = Thread()
+                self.thread.finished.connect(self.on_finished)
+                self.thread.start()
+
+        elif self.listRadio.isChecked():
+            pass
+        else:
+            QMessageBox.information(self, 'Information', f'Choosing Single or List Domain')
+
+    def on_finished(self):
+        self.reconbtn.setEnabled(True)
     def open_file_dialog(self):
-        project_name: QtWidgets.QLineEdit = self.projectName.text()
+        project_name: QLineEdit = self.projectName.text()
         file_dialog = QFileDialog()
         file_dialog.setFileMode(QFileDialog.DirectoryOnly)
         file_dialog.setOption(QFileDialog.ShowDirsOnly, True)
@@ -34,10 +76,10 @@ class MainWindow(QMainWindow):
             QMessageBox.information(self, 'Information', f'write project name correct.')
 
     def do_work(self):
-        project_name: QtWidgets.QLineEdit = self.projectName
+        project_name: QLineEdit = self.projectName
         stack_widget: QStackedWidget = self.stackedWidget
-        waf_widget: QtWidgets.QRadioButton = self.WafRadio
-        frame_widget: QtWidgets.QRadioButton = self.FrameRadio
+        waf_widget: QRadioButton = self.WafRadio
+        frame_widget: QRadioButton = self.FrameRadio
         if waf_widget.isChecked():
             stack_widget.setCurrentIndex(1)
         elif frame_widget.isChecked():
@@ -53,19 +95,23 @@ class MainWindow(QMainWindow):
             QMessageBox.warning(self, 'Warning', 'Choose Option')
 
     def back_home(self):
-        waf_widget: QtWidgets.QRadioButton = self.WafRadio
-        frame_widget: QtWidgets.QRadioButton = self.FrameRadio
-        self.widget.hide()
-        waf_widget.setChecked(False)
-        frame_widget.setChecked(False)
-        stack_widget: QStackedWidget = self.stackedWidget
-        stack_widget.setCurrentIndex(0)
+        # waf_widget: QtWidgets.QRadioButton = self.WafRadio
+        # frame_widget: QtWidgets.QRadioButton = self.FrameRadio
+        self.stackedWidget.setCurrentIndex(0)
+        self.chooseProject.hide()
+        # waf_widget.setChecked(False)
+        # frame_widget.setChecked(False)
 
 
-app = QApplication([])  # Start App
-main_app = MainWindow()
-main_app.show()
-try:
-    sys.exit(app.exec_())  # Exit app when Press X
-except Exception as error:
-    print(error)
+def main():
+    app = QApplication([])  # Start App
+    main_app = MainWindow()
+    main_app.show()
+    try:
+        sys.exit(app.exec_())  # Exit app when Press X
+    except Exception as error:
+        print(error)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Recon/recon.py
@@ -0,0 +1,95 @@
+import os
+import platform
+
+
+####windows
+# cwd=os.path.dirname(__file__) get dir
+# os.system(f'cd {cwd}')
+# os.system(f'"{cwd}\wsubfinder.exe"') exe script
+# platform.system()
+
+def subfinder_for_single_windows(Domain):  # single domain (collect subdomain)
+    cwd = os.path.dirname(__file__)
+    print(cwd)
+    # os.system(f'{cwd}\wsubfinder.exe -d {Domain} -o domains.txt')
+    os.system(fr'{cwd}\wsubfinder.exe -d "{Domain}"  >>{cwd}\\domains.txt')
+
+
+def subfinder_for_file_windows(path):  # list domain (collect subdomain)
+    cwd = os.path.dirname(__file__)
+    os.system(f'{cwd}\wsubfinder.exe -dL {path}  >>{cwd}\\domains.txt')
+
+
+def subfinder_single_linux(Domain):
+    os.system(f'subfinder -d {Domain} >>domains')
+
+
+def subfinder_multi_linux(path):
+    os.system(f'subfinder -dL {path} >>domains')
+
+
+def httprobe_w():  # live domain
+    cwd = os.path.dirname(__file__)
+    os.system(f"type domains.txt | {cwd}\whttprobe.exe >>{cwd}\\urls.txt")
+
+
+def httprobe_l():
+    os.system("cat domains |httprobe >>urls")
+
+
+def screenwin():  # screenshot
+    cwd = os.path.dirname(__file__)
+    os.system(f"type urls.txt | {cwd}\waquatone.exe -chrome-path chrome.exe")
+
+
+def screenlinux():
+    os.system("cat urls | aquatone ")
+
+
+def wwayback():  # endpoint, JS, Parameter
+    cwd = os.path.dirname(__file__)
+    os.system(f'type domains.txt | {cwd}\wwaybackurls.exe >>archive.txt')
+
+
+def Js_file():
+    pass
+
+
+def Parameter():
+    pass
+
+
+def lwayback():
+    os.system('cat domains | waybackurls >>archive')
+
+
+def main():
+    print("""
+
+     1-single target
+     2-list of targer
+
+
+
+
+    """)
+    choose = input()
+    if (choose == "1"):
+        Domain = input("Enter target:")
+        if (platform.system() == "Windows"):
+            print("windows")
+
+        elif (platform.system() == "Linux"):
+            print("Linux")
+        ## subfinder_single_linux(Domain)
+    else:
+        path = input("Enter file path:")
+
+    if (platform.system() == "Windows"):
+        print("windows")
+    elif (platform.system() == "Linux"):
+        print("Linux")
+
+
+if __name__ == '__main__':
+    main()
